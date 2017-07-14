ee45f82dbc60bd55e30eac9ba65f95ff34c60510
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2017-07-14 10:59:55 +0200

    expand analyzer

--- index.js
+++ index.js
@@ -5,42 +5,13 @@
 
 const fs = require('fs');
 const validate = require('./lib/validate.js');
-const analyze = require('./lib/analyze.js');
+const Analyzer = require('./lib/analyzer.js');
 const N3 = require('n3');
+const namespaces = require('prefix-ns').asMap();
 
-let mappingsFolder = process.argv[2];
+let input = process.argv[2];
 
-console.log(mappingsFolder);
-
-let mapping = `@prefix rr: <http://www.w3.org/ns/r2rml#> .
-@prefix foaf: <http://xmlns.com/foaf/0.1/> .
-@prefix rml: <http://semweb.mmlab.be/ns/rml#> .
-@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
-@prefix ql: <http://semweb.mmlab.be/ns/ql#> .
-
-@base <http://www.example.com/> .
-
-<#Mapping> a rr:TriplesMap;
-  rml:logicalSource [
-    rml:source "data.csv";
-    rml:referenceFormulation ql:CSV
-  ];
-
-  rr:subjectMap <#SubjectMap> ;
-  rr:predicateObjectMap <#PredicateObjectMap> .
-
-<#PredicateObjectMap> a rr:PredicateObjectMap;
-  rr:predicateMap <#PredicateMap>;
-  rr:objectMap <#ObjectMap1>.
-
-<#PredicateMap> a rr:PredicateMap;
-  rr:constant foaf:age.
-
-<#ObjectMap1> a rr:ObjectMap;
-  rml:reference "Age" ; rr:datatype xsd:float .
-
-<#SubjectMap> a rr:SubjectMap;
-  rr:template "http://example.com/{Name}_{Surname}".`;
+let mapping = fs.readFileSync(input, 'utf-8');
 
 validate(mapping).then((store) => {
   let parser = N3.Parser();
@@ -50,7 +21,9 @@ validate(mapping).then((store) => {
     if (triple) {
       mappingStore.addTriple(triple.subject, triple.predicate, triple.object);
     } else {
-      analyze(store, mappingStore);
+      let analyzer = new Analyzer(store, mappingStore);
+      analyzer.analyze();
+      //console.log(analyzer.getEffectsOfSingleChange('http://www.example.com/#ObjectMap1', namespaces.rr + 'datatype'));
     }
   });
 });
